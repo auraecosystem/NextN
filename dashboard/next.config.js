019873e1ae0d6ea8de540e3a5c1fe7d2d4e875db
Seriki Walter Yakub <webapp4@outlook.com>
2026-08-04 03:24:10 +0100

    Create next.config.js

---
+++ dashboard/next.config.js
@@ -0,0 +1,11 @@
+// dashboard/next.config.js
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/:path*',
+      },
+    ]
+  },
+}
